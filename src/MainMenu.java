64e0ddd8ddc2d3a57dca1411a2786c82eddbc21a
jayeuse <ceejay.feruelo@gmail.com>
2024-10-29 12:39:47 +0800

    Creating Exercise 4 Repository

---
+++ src/MainMenu.java
@@ -0,0 +1,48 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MainMenu {
+    Scanner sc = new Scanner(System.in);
+    Operations operations = new Operations();
+
+    public void createMenu () {
+
+        while (true){
+
+            System.out.println("Stacks Operations");
+            System.out.println("Conversions");
+            System.out.println("1. Infix to Postfix");
+            System.out.println("2. Infix to Prefix");
+            System.out.println("3. Postfix to Prefix");
+            System.out.println("0. Stop");
+            System.out.print("Enter Choice: ");
+
+            try{
+
+                int choice = sc.nextInt();
+
+                switch (choice){
+                    case 1:
+                        operations.Infix_to_Postfix();
+                        break;
+                    case 2:
+                        operations.Infix_to_Prefix();
+                        break;
+                    case 3:
+                        operations.Postfix_to_Prefix();
+                        break;
+                    case 0:
+                        System.out.println("Ending Program");
+                        return;
+                    default:
+                        System.out.println("Error: Please Enter a valid choice!");
+                        break;
+                }
+
+            }catch (InputMismatchException e) {
+                System.out.println("Error: Please Enter a valid choice!");
+                sc.next();
+            }
+        }
+    }
+}
---
+++ src/Operations.java
@@ -0,0 +1,14 @@
+public class Operations {
+
+    public void Infix_to_Postfix(){
+        System.out.println("Infix to Postfix Operation");
+    }
+
+    public void Infix_to_Prefix(){
+        System.out.println("Infix to Prefix Operation");
+    }
+
+    public void Postfix_to_Prefix(){
+        System.out.println("Postfix to Prefix Operation");
+    }
+}
