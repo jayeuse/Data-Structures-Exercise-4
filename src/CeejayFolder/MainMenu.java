870adc253e427bb952edd67de313c85813c36c12
Ceejay Feruelo <ceejay.feruelo@gmail.com>
2024-11-02 14:18:54 +0000

    Created Postfix to Prefix, added ClearScreen -Ceejay

--- src/CeejayFolder/MainMenu.java
+++ src/CeejayFolder/MainMenu.java
@@ -7,6 +7,8 @@ public class MainMenu {
     Scanner sc = new Scanner(System.in);
     Operations operations = new Operations();
 
+    String filler;
+
     public void createMenu () {
 
         while (true){
@@ -31,7 +33,32 @@ public class MainMenu {
                         operations.Infix_to_Prefix();
                         break;
                     case 3:
-                        operations.Postfix_to_Prefix();
+
+                        boolean isRunning = true;
+
+                        do {
+
+                            operations.Postfix_to_Prefix();
+
+                            String tryAgain;
+                            while (true) {
+                                System.out.print("Try Again? (Y/N): ");
+                                tryAgain = sc.nextLine().trim();
+
+                                if (tryAgain.equalsIgnoreCase("y") || tryAgain.equalsIgnoreCase("n")) {
+                                    clearScreen();
+                                    break;
+                                } else {
+                                    System.out.println("Invalid input. Please enter 'Y' or 'N'.");
+                                }
+                            }
+
+                            if (tryAgain.equalsIgnoreCase("n")) {
+                                isRunning = false;
+                            }
+
+                        } while (isRunning);
+
                         break;
                     case 0:
                         System.out.println("Ending Program");
@@ -46,5 +73,23 @@ public class MainMenu {
                 sc.nextLine();
             }
         }
+    }
+
+    public void clearScreen(){
+        System.out.println("Enter to Continue");
+        filler = sc.nextLine();
+
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- src/CeejayFolder/Operations.java
+++ src/CeejayFolder/Operations.java
@@ -1,6 +1,7 @@
 package CeejayFolder;
 
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Operations {
     Scanner sc = new Scanner(System.in);
@@ -15,5 +16,69 @@ public class Operations {
 
     public void Postfix_to_Prefix(){
         System.out.println("Postfix to Prefix Operation");
+
+        System.out.print("Enter a Postfix Expression: ");
+        String postfix = sc.nextLine();
+
+        if (!isValidPostfix(postfix)) {
+            System.out.println("Invalid Postfix Expression!");
+            return;
+        }
+
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < postfix.length(); i++) {
+            char characterExpression = postfix.charAt(i);
+
+            if (Character.isWhitespace(characterExpression)) {
+                continue;
+            }
+
+            if (Character.isLetterOrDigit(characterExpression)) {
+                stack.push(String.valueOf(characterExpression));
+            } 
+
+            else if (isOperator(characterExpression)) {
+
+                String operand2 = stack.pop();
+                String operand1 = stack.pop();
+
+                String prefix = characterExpression + operand1 + operand2;
+
+                stack.push(prefix);
+            } else {
+                System.out.println("Invalid character encountered in expression.");
+                return;
+            }
+        }
+
+        String prefixExpression = stack.pop();
+        System.out.println("Prefix expression: " + prefixExpression);
+    }
+
+    private boolean isValidPostfix(String postfix) {
+        int operandCount = 0;
+        for (int i = 0; i < postfix.length(); i++) {
+            char expressionCharacter = postfix.charAt(i);
+
+            if (Character.isWhitespace(expressionCharacter)) {
+                continue;
+            }
+            if (Character.isLetterOrDigit(expressionCharacter)) {
+                operandCount++;
+            } else if (isOperator(expressionCharacter)) {
+                if (operandCount < 2) {
+                    return false;
+                }
+                operandCount--;
+            } else {
+                return false;
+            }
+        }
+        
+        return operandCount == 1;
+    }
+
+    private boolean isOperator(char opertorCharacter) {
+        return opertorCharacter == '+' || opertorCharacter == '-' || opertorCharacter == '*' || opertorCharacter == '/' || opertorCharacter == '^';
     }
 }
