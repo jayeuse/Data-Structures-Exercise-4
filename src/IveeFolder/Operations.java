dd4f253474015bc25ef11df477c3c8766cabcd14
Ivee Guevarra <186171197+eiko-glitch@users.noreply.github.com>
2024-11-03 13:43:50 +0000

    Infix_to_Postfix

--- src/IveeFolder/Operations.java
+++ src/IveeFolder/Operations.java
@@ -7,75 +7,91 @@ public class Operations {
     Scanner sc = new Scanner(System.in);
 
     public void Infix_to_Postfix(){
-        boolean ContinueConversion = true;
+        boolean continueConversion = true;
+    
+    while(continueConversion){
+        try{
 
-        while(ContinueConversion){
-            try{
+            System.out.println("-------------------------* * *-------------------------");
             System.out.println("Infix to Postfix Operation");
-            System.out.println("Enter an Infix Expression: ");
-                String InfixEx = sc.nextLine();
+            System.out.println("-------------------------* * *-------------------------");
+            System.out.print("Enter Infix Expression: ");
+                String infix = sc.nextLine();
 
-                String PostfixEx = InfixtoPostfix(InfixEx);
-            System.out.println("From Infix to PostFIX Expression: " + PostfixEx);
+                infix = infix.toUpperCase();           
+                infix = infix.replaceAll("\\s+", ""); 
 
-    }   catch (Exception e){
-        System.out.println("Error! Please enter a valid INFIX EXPRESSION.");
-        System.out.print("Do you want to convert another expression? (Y/N): ");
-            String UserInput = sc.nextLine().trim().toLowerCase();
+                if (infix.matches(".*\\d.*")) { 
+                    System.out.println("Error: Numbers are not allowed in the expression!");
+                    continue; 
+                }
+                System.out.println("-------------------------* * *-------------------------");
+            System.out.println("Infix Expression: " + infix);
+            
+                String postfix = InToPos(infix);
+                System.out.println("-------------------------* * *-------------------------");
+            System.out.println("Coverted expression from Infix to POSTFIX: " + postfix);
+            System.out.println("-------------------------* * *-------------------------");
+            }catch (Exception e){
+                System.out.println("Error: Please enter a valid INFIX EXPRESSION!");
+                sc.nextLine();
+            }  
 
-        if (!UserInput.equals("yes")){
-                ContinueConversion = false;
-                    System.out.println("You are now exiting!");
-                
+                System.out.print("Do you want to convert another expression? (Y/N): ");
+                String UserInput = sc.nextLine().trim().toUpperCase().toLowerCase();
+    
+                if (!UserInput.equals("Y")) {
+                    continueConversion = false;
+                    System.out.println("Thank you for using the program!");
+                }
             }
+    
+            sc.close();
+        }  
+        private String InToPos(String infix) {
+
+            String postfix = "";
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < infix.length(); i++) {
+                char ch = infix.charAt(i);
+                if (ch == '(') {
+                    stack.push(ch);
+                } else if (ch == ')') {
+                    while (!stack.isEmpty() && stack.peek() != '(') {
+                        postfix += stack.pop();
+                    }
+                    stack.pop();
+                } else if (Character.isLetterOrDigit(ch)) {
+                    postfix += ch;
+                } else {
+                    while (!stack.isEmpty() && precedence(ch) <= precedence(stack.peek())) {
+                        postfix += stack.pop();
+                    }
+                    stack.push(ch);
+                }
+            }
+    
+            while (!stack.isEmpty()) {
+                postfix += stack.pop();
+            }
+    
+            return postfix;
         }
-        sc.close();
-    }  
-}  private String InfixtoPostfix(String InfixEx) throws Exception {
-    String PostfixEx = "";
-    Stack<Character> stack = new Stack<>();
-
-    for (int i = 0; i < InfixEx.length(); i++) {
-        char ch = InfixEx.charAt(i);
-        if (ch == '(') {
-            stack.push(ch);
-        } else if (ch == ')') {
-            while (!stack.isEmpty() && stack.peek() != '(') {
-                PostfixEx += stack.pop();
-                    throw new Exception("Invalid expression");
+    
+        private int precedence(char operator) {
+            switch (operator) {
+                case '+':
+                case '-':
+                    return 1;
+                case '*':
+                case '/':
+                    return 2;
+                case '^':
+                    return 3;
             }
-            stack.pop();
-        } else if (Character.isLetterOrDigit(ch)) {
-            PostfixEx += ch;
-        } else {
-            while (!stack.isEmpty() && precedence(ch) <= precedence(stack.peek())) {
-                PostfixEx += stack.pop();
-            }
-            stack.push(ch);
-        throw new Exception("Invalid character in expression: " + stack.peek());
-        }       
-        
-    }
-
-    while (!stack.isEmpty()) {
-        PostfixEx += stack.pop();
-    }
-
-    return PostfixEx;
-}
-    private int precedence(char operator) {
-        switch (operator) {
-            case '+':
-            case '-':
-                return 1;
-            case '*':
-            case '/':
-                return 2;
-            case '^':
-                return 3;
-        }
         return -1;
- }
+
+    }
 
 
     public void Infix_to_Prefix(){
